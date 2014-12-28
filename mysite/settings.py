acfa7c50cd67d97e63d33fb2f8d70fe698a6757d
Katie Li <katiesli16@gmail.com>
2014-12-28 11:10:42 -0700

    rollback

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,6 +4,7 @@ Django settings for mysite project.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
 
+
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """

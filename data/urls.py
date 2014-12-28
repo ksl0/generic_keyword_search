f89bb15eaafcc5151a0e687b1f428f1f93c95feb
Katie Li <katiesli16@gmail.com>
2014-12-28 11:22:54 -0700

    revert

--- data/urls.py
+++ data/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import patterns, url
-from django.conf import settings
+
 from data import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index,name='index')
    ) 
-
-
--- data/views.py
+++ data/views.py
@@ -19,18 +19,18 @@ def index(request):
 	topic1.encode("ascii",'ignore')
 	total_twitter_output = settings.T_KEY.search(q=topic1, count=n_results)
 	tweet_list = []   
-        user_list = []
+
 	
 	for i in xrange(0,n_results): 
 	# call a filtering function here      
 	    text = total_twitter_output['statuses'][i]['text'].encode("ascii", 'ignore')
 	    username = total_twitter_output['statuses'][i]['user']['screen_name'].encode("ascii", 'ignore')
-	    tweet_list.append([username, text])
+	    tweet_list.append((username, text))
 	    #response.write("<div><p> username </p><p> text </p></div>")
 	return tweet_list 
 
     context = RequestContext(request, {
-             'datapoints':  unwritten(KeyWords, 47)
+              'topics': unwritten(KeyWords, 10)
     })
    
     return HttpResponse(template.render(context))
